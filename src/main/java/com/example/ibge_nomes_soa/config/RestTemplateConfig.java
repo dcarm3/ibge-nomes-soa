54e748bf21c048900a855a0ad21b2e59d58efa7f
dcarm3 <dcardosom3@gmail.com>
2025-05-13 21:02:30 -0300

    Commit inicial

---
+++ src/main/java/com/example/ibge_nomes_soa/config/RestTemplateConfig.java
@@ -0,0 +1,13 @@
+package com.example.ibge_nomes_soa.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class RestTemplateConfig {
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
---
+++ src/test/java/com/example/ibge_nomes_soa/IbgeNomesSoaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ibge_nomes_soa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IbgeNomesSoaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
